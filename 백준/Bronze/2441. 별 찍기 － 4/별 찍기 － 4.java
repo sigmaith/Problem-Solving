8296685211b5ce83b7ff013e120d31a0666c2d03
Smith <125195487+sigmaith@users.noreply.github.com>
2024-02-03 16:49:08 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 396 ms, Memory: 22936 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int i, j;
+
+        for( i=0; i<N; i++){
+            for (j=0; j<N; j++){
+                if(j>=i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
