20e54676bba3dab1b055264ede52d145aaa6e299
Smith <125195487+sigmaith@users.noreply.github.com>
2024-02-08 21:39:47 +0900

    [Silver I] Title: 효율적인 해킹, Time: 16852 ms, Memory: 210488 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+A = [[] for _ in range(N+1)]
+answer = [0] * (N+1)
+
+def BFS(v):
+    visited = [0] * (N + 1)
+    queue = deque([v])
+    visited[v] = 1
+    while queue:
+        now_Node = queue.popleft()
+        for i in A[now_Node]:
+            if not visited[i]:
+                visited[i] = 1
+                answer[i] += 1
+                queue.append(i)
+
+for _ in range(M):
+    S, E = map(int, input().split())
+    A[S].append(E)
+
+for i in range(1, N+1):
+    if len(A[i]) != 0:
+        BFS(i)
+
+maxVal = max(answer)
+for i in range(1, N+1):
+    if maxVal == answer[i]:
+        print(i, end=' ')
