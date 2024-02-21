92c03012391cc13b2c1c4de4df166fb0a2fea746
5mith <125195487+sigmaith@users.noreply.github.com>
2024-02-21 11:54:39 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 424 ms, Memory: 209552 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,22 +1,20 @@
 import sys
-
 input = sys.stdin.readline
 
 N = int(input())
 A = list(map(int, input().split()))
-dp_table = [[0] * 10001 for _ in range(N)]
-
+dp_table = [[0] * 10001 for _ in range(N)] #dp_table에는 find(idx,num)값을 다시 연산하지 않도록 값을 기록
 
 # 지금 이 수를 선택을 할지 말지 결정하는 로직이 필요
 # 재귀의 단계단계에서는 현재 수를 선택 or 선택 x
 
-def find(idx, num):  # idx: traverse A, num: before number
+def find(idx, num):  # idx: traverse A, num: before number(선택한 수열의 가장 이전 최신값)
     if idx == N:
         return 0
     else:
         if dp_table[idx][num] == 0:
             if A[idx]>num:
-                result = max(0 + find(idx + 1, num), 1 + find(idx + 1, A[idx]))
+                result = max(0 + find(idx + 1, num), 1 + find(idx + 1, A[idx]))  #선택 안하고 넘기기 / 선택하고 넘기기
             else:
                 result = 0 + find(idx+1, num)
             dp_table[idx][num] = result
