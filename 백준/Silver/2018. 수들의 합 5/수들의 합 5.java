5d951faa0c2bb9bbd9fc92ef7f17c872ee801725
5mith <125195487+sigmaith@users.noreply.github.com>
2024-07-01 20:17:37 +0900

    [Silver V] Title: 수들의 합 5, Time: 220 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -10,18 +10,11 @@ public class Main {
     static int result;
     static void solve(int startNum) throws Exception{
         int num = startNum;
-
-        //bw.write("startNum = " + startNum + "\n");
         for(int i=startNum+1; ;i++){
             num += i;
             if (num > n) break;
             else if (num == n){
                 result += 1;
-
-                //
-                
-                //
-
                 break;
             }
         }
