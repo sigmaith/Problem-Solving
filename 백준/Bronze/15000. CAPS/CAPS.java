cb7006d0747ef12704847fbda485f44be012187b
5mith <125195487+sigmaith@users.noreply.github.com>
2024-07-02 20:47:16 +0900

    [Bronze V] Title: CAPS, Time: 376 ms, Memory: 27516 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        
+        System.out.println(input.toUpperCase());
+    }
+}
