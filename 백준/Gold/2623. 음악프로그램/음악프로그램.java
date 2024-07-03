dd34bc0dbef6206a7b02ab132af4b7774c660f68
5mith <125195487+sigmaith@users.noreply.github.com>
2024-07-03 11:09:56 +0900

    [Gold III] Title: 음악프로그램, Time: 136 ms, Memory: 15040 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.java
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.java
@@ -8,26 +8,24 @@ public class Main {
     static StringTokenizer st;
     static List<List<Integer>> graph;
     static int[] in_degree;
-    static int[] desc;
     private static void topological_sort(int n)throws Exception{
         List<Integer> result = new ArrayList<>();
-        Deque<Integer> q = new ArrayDeque<>();
+        Deque<Integer> q = new ArrayDeque<>(); // Deque를 큐처럼 쓰기
         for (int i=1; i<n+1; i++){
-            if (in_degree[i] == 0) q.addLast(i);
+            if (in_degree[i] == 0) q.addLast(i); // 진입차수 0인것을 큐에 넣기
         }
         while (!q.isEmpty()){
-            int now = q.pollFirst();
+            int now = q.pollFirst(); // q에서 뽑아서 
             result.add(now);
-            for (int ele: graph.get(now)){
-                in_degree[ele] -= 1;
-                if (in_degree[ele] == 0){
-                    q.addLast(ele);
+            for (int ele: graph.get(now)){ // 인접 행렬 탐색하며 
+                in_degree[ele] -= 1; // 진입차수 갱신
+                if (in_degree[ele] == 0){ 
+                    q.addLast(ele); // 0이면 큐에 넣기
                 }
             }
         }
-
         if(result.size() != n) {
-            bw.write("0\n");
+            bw.write("0\n"); // 사이클이 존재할 시, 순서랄것이 없어 size 가 vertice개수보다 작아짐.
         }else{
             for (int element : result) {
                 bw.write(element + "\n");
@@ -40,20 +38,16 @@ public class Main {
         st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken()); // vertice
-        int m = Integer.parseInt(st.nextToken()); // pd 개수
+        int m = Integer.parseInt(st.nextToken()); // pd 명수 (엣지를 제시해줄 것임)
         int flag = 0;
 
         graph = new ArrayList<>();
         for (int i = 0; i < n + 1; i++){
             List<Integer> array = new ArrayList<>();
             graph.add(array);
-        } // graph 동적 2차원 정수(Integer) 배열 초기화
+        } // adjacent list(인접리스트) 생성
 
         in_degree = new int[n + 1]; // 진입차수: 자기 자신을 가리키는 엣지의 개수
-        desc = new int[n + 1]; // 조상기록 
-        for(int i=1; i<n+1; i++){
-            desc[i] = i;
-        }
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine()); 
             int num = Integer.parseInt(st.nextToken()); // vertice 개수
@@ -63,7 +57,7 @@ public class Main {
             }
             for (int j=1; j<num; j++){
                 graph.get(numList[j-1]).add(numList[j]); // graph 시작점 배열에 도착점 넣기
-                in_degree[numList[j]] += 1;
+                in_degree[numList[j]] += 1; // 진입 차수 갱신
             }
         }
         if (flag == 0)topological_sort(n);
