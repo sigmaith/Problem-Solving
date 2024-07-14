9296c8767d4519e180b57cadfd5df6b036256314
5mith <125195487+sigmaith@users.noreply.github.com>
2024-07-14 18:14:10 +0900

    [Silver V] Title: 수들의 합 5, Time: 136 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,39 +1,41 @@
-// #2018 수들의 합5
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static BufferedReader br;
-    static BufferedWriter bw;
-    static StringTokenizer st;
-    static int n;
-    static int result;
-    static void solve(int startNum) throws Exception{
-        int num = startNum;
-        for(int i=startNum+1; ;i++){
-            num += i;
-            if (num > n) break;
-            else if (num == n){
-                result += 1;
-                break;
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // 작으면 tail을 키우고, 크면 head를 앞으로
+        int target = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        int sum = 1;
+        int head = 1; int tail = 2;
+        while (head <= target) {   // 최적화 가능
+            if (sum < target) {
+                sum += tail ++;
+            } else if (sum > target) {
+                sum -= head ++;
+            } else {
+                answer ++;
+                // System.out.println(head + " ~ " + tail);
+                sum += tail ++;
             }
-        }
-    }
 
-    public static void main(String[] args) throws Exception{
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        result = 0;
-        for (int i=1; i<=n; i++){
-            solve(i);
         }
 
-        result += 1;
-        bw.write(result + "\n");
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
+
     }
 }
+
+/* 
+9   = 4 + 5
+    = 2 + 3 + 4
+
+15  = 1 + 2 + 3 + 4 + 5
+    = 4 + 5 + 6
+    = 7 + 8
+*/
