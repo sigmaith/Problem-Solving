598bd32355d118911c1aa223736a777788195d8d
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-08-07 14:53:58 +0900

    [Gold III] Title: 나머지 합, Time: 556 ms, Memory: 133344 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,44 @@
+// 10986번: 나머지 합
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.valueOf(st.nextToken());
+        int m = Integer.valueOf(st.nextToken());
+
+        long[] arr = new long[n];
+        long[] sum = new long[n];
+        long[] remainder = new long[m];
+        long result = 0;
+
+        st = new StringTokenizer(br.readLine());
+        long s = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.valueOf(st.nextToken()); // 원본 배열 저장
+            s += arr[i]; sum[i] = s; // 합 배열 저장
+            int mod = (int)(s % m); 
+            remainder[mod]++; // 나머지 개수 갱신 
+            if (mod == 0) {
+                result += 1;
+            }
+        }
+        // System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(sum));
+        // System.out.println(Arrays.toString(remainder));
+        
+        for (int i = 0; i < m; i++) {
+            if (remainder[i] > 1) {
+                result += (remainder[i] * (remainder[i] - 1)) / 2; // 0개가 아니면 nC2 연산
+            }
+        }
+        bw.write(result + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
