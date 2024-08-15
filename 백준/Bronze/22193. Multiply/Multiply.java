84a46e5ae1a6b9eec7473ec37a0eabaaa40b0930
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-08-15 18:10:53 +0900

    [Bronze V] Title: Multiply, Time: 580 ms, Memory: 55040 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 public class Main{
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
-        long n = Long.valueOf(br.readLine());
-        long m = Long.valueOf(br.readLine());
-        System.out.println(n * m);
+        BigInteger a = new BigInteger(br.readLine());
+		BigInteger b = new BigInteger(br.readLine());
+        System.out.println(a.multiply(b));
     }
 }
