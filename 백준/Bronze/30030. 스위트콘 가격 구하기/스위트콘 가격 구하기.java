65aee125a9d88d378db0cd68e7959ca0afbc8e4d
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-09-23 23:16:20 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 172 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,13 @@
+// boj 30030
+
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		System.out.println(n - n / 11);
+		sc.close();
+	}
+}
