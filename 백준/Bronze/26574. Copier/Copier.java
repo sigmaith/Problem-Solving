fc1b8fb16c91a8107fbed0ab7369f42c858e1b5a
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-10-04 23:52:18 +0900

    [Bronze V] Title: Copier, Time: 180 ms, Memory: 18352 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int testCase = sc.nextInt();
+        for (int i = 0; i < testCase; i++) {
+            int origNum = sc.nextInt();
+            System.out.println(origNum + " " + origNum);
+        }
+        
+    }
+}
