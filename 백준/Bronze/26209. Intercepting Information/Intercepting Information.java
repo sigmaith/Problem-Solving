763abad4477a97c814d531199c62842db9d83d80
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-10-09 22:25:00 +0900

    [Bronze V] Title: Intercepting Information, Time: 100 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/26209. Intercepting Information/Intercepting Information.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            if (st.nextToken().equals("9")) {
+                System.out.println("F");
+                return;
+            }
+        }
+        System.out.println("S");
+    }
+}
