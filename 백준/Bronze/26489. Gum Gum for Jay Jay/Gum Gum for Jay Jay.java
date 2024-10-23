d7e4c25ef9645409cf66c6a5e6d309eaf6b5e571
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-10-23 22:25:58 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        while (true) {
+            String tmp = br.readLine();
+            if (tmp == null) break;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
