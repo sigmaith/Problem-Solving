96c94867f54d27dd5e26c3224049c29805f07512
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-10-27 20:11:53 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 180 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.java
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.java
@@ -1,40 +1,32 @@
+// boj 29731 2033년 밈 투표
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] check = {
-            "Never gonna give you up",
-            "Never gonna let you down",
-            "Never gonna run around and desert you",
-            "Never gonna make you cry",
-            "Never gonna say goodbye",
-            "Never gonna tell a lie and hurt you",
-            "Never gonna stop"
-        };
-        
-        int T = Integer.parseInt(br.readLine().trim());
-        boolean found = false;
-        
-        while (T-- > 0) {
-            String s = br.readLine();
-            boolean matchFound = false;
-            
-            for (int i = 0; i < 7; i++) {
-                if (s.equals(check[i])) {
-                    matchFound = true;
-                    break;
-                }
-            }
-            
-            if (!matchFound) {
-                System.out.println("Yes");
-                return;
+        Set<String> pledges = new HashSet<>();
+        pledges.add("Never gonna give you up");
+        pledges.add("Never gonna let you down");
+        pledges.add("Never gonna run around and desert you");
+        pledges.add("Never gonna make you cry");
+        pledges.add("Never gonna say goodbye");
+        pledges.add("Never gonna tell a lie and hurt you");
+        pledges.add("Never gonna stop");
+
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        boolean flag = false;
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine().strip();
+            if (!pledges.contains(str)) {
+                flag = true;
             }
         }
-        
-        System.out.println("No");
-        br.close();
+
+        if (flag) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
