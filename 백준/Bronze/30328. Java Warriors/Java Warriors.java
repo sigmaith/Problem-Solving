751fd5d806bd7201be6c09af5ecf05b0d412f2aa
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-10-30 23:28:45 +0900

    [Bronze V] Title: Java Warriors, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,10 @@
+// Java Warriors
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n * 4000);
+    }
+}
