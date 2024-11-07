ce4e1e63b1a3a15f0edc9cc489b796060fb243f1
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-11-07 10:41:37 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 100 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.println(x + y + 3);
+    }
+}
