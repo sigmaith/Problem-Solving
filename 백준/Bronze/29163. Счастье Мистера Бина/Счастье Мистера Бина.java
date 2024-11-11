92726f26dba54f723c52b1721427f8ff7b84e338
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-11-11 23:20:22 +0900

    [Bronze V] Title: Счастье Мистера Бина, Time: 220 ms, Memory: 19616 KB -BaekjoonHub

---
+++ 백준/Bronze/29163. Счастье Мистера Бина/Счастье Мистера Бина.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int even = 0;
+		int odd = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			if(a % 2 == 0) {
+				even++;
+			}else {
+				odd++;
+			}
+		}
+		
+		if(even > odd) {
+			System.out.println("Happy");
+		}else {
+			System.out.println("Sad");
+		}
+		sc.close();
+	}
+}
