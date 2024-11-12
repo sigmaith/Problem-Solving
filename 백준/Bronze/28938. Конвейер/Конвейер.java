5fae1708670a578618b64aa689ab6f7df8efc1b3
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-11-12 23:55:30 +0900

    [Bronze V] Title: Конвейер, Time: 232 ms, Memory: 20628 KB -BaekjoonHub

---
+++ 백준/Bronze/28938. Конвейер/Конвейер.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int total = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			if(a == -1) {
+				total = total - 1;
+			}else if(a == 1) {
+				total = total + 1;
+			}
+		}
+		
+		if(total > 0) {
+			System.out.println("Right");
+		}else if(total < 0) {
+			System.out.println("Left");
+		}else if(total == 0) {
+			System.out.println("Stay");
+		}
+		sc.close();
+	}
+}
