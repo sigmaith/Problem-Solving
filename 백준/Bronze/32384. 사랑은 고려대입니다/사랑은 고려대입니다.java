63a4843325cd51671325c4ee8445c9496aacd08b
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-11-15 20:39:26 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+		    sb.append("LoveisKoreaUniversity ");
+		}
+		System.out.println(sb.toString());
+	}
+}
