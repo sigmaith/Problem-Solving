c8a96ffc88cb9b3c855ee01c680a1ebaee82f0da
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-11-27 16:00:09 +0900

    [Gold IV] Title: 서강그라운드, Time: 128 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -0,0 +1,72 @@
+// boj 14938 서강그라운드
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 지역 개수 (정점)
+        int m = Integer.parseInt(st.nextToken()); // 수색 범위
+        int r = Integer.parseInt(st.nextToken()); // 길의 개수 (엣지)
+
+        int[] regionItems = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int region = 1; region <= n; region++) {
+            regionItems[region] = Integer.parseInt(st.nextToken());
+        }
+
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < r; i++) { // 간선 입력
+            st = new StringTokenizer(br.readLine());
+            int regA = Integer.parseInt(st.nextToken());
+            int regB = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            adj.get(regA).add(new int[]{regB, cost});
+            adj.get(regB).add(new int[]{regA, cost}); // 양방향
+        }
+
+        int maxItems = -1;
+        for (int src = 1; src <= n; src++) {
+            int[] dist = new int[n + 1];
+            for (int region = 1; region <= n; region++) {
+                dist[region] = Integer.MAX_VALUE;
+            } // 지역마다 거리 초기화 필요
+
+            dist[src] = 0;
+            PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> {return Integer.compare(o1[1], o2[1]);})); // cost 비교
+            pq.offer(new int[]{src, 0});
+            while (!pq.isEmpty()) {
+                int[] now = pq.poll();
+                int nowReg = now[0];
+                int nowCost = now[1];
+                if (dist[nowReg] < nowCost) {
+                    continue;
+                }
+                for (int[] info: adj.get(nowReg)) {
+                    int nextReg = info[0];
+                    int cost = info[1];
+                    if (dist[nextReg] > nowCost + cost) {
+                        dist[nextReg] = nowCost + cost; // 업데이트
+                        pq.offer(new int[]{nextReg, dist[nextReg]});
+                    }
+                }
+            } // 지역마다 dijkstra ~ing
+
+            int items = 0;
+            for (int i = 1; i <= n; i++) {
+                if (dist[i] <= m) {
+                    items += regionItems[i];
+                }
+            }
+            if (maxItems < items) {
+                maxItems = items;
+            }
+        }
+        System.out.println(maxItems);
+    }
+}
