3b9617de2533ac0dd9e1901ce6273f1318c3acac
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-12-04 17:37:16 +0900

    [Gold III] Title: 소수의 연속합, Time: 268 ms, Memory: 21540 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,66 @@
+// boj 1644 소수의 연속합
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        boolean[] prime = new boolean[N + 1];
+        deleteNonPrimeNumbers(prime, N);
+        System.out.println(getContinuousPrimeNumSum(prime, N));
+    }
+
+    private static void deleteNonPrimeNumbers(boolean[] prime, int N) {
+        Arrays.fill(prime, true);
+        prime[0] = false;
+        prime[1] = false;
+
+        for (int i = 2; i < N; i++) {
+            if (!prime[i]) {
+                continue;
+            }
+            for (int j = i * 2; j <= N; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    private static int getContinuousPrimeNumSum(boolean[] prime, int N) {
+        int result = 0, A = 2, B = 0;
+        // pointer a <= pointer b
+        // 종료조건: 배열[a] 가 N에 도달했을 경우 추가하고 종료..
+        int sum = 0;
+        while (true) {
+            if ((B == N && sum == N) || B > N) {
+                break;
+            }
+            while (sum < N && B <= N) {
+                B++;
+                if (B <= N && prime[B]) {
+                    sum += B;
+                }
+            } // sum 은 큰 상태
+            while (sum > N && A <= N) {
+                if (A <= N && prime[A]) {
+                    sum -= A;
+                }
+                A++;
+            }
+            if (sum == N) {
+                result += 1;
+                // 균형깨뜨려주기
+                while (B < N) {
+                    B++;
+                    if (B <= N && prime[B]) {
+                        sum += B;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
+
