05fd4796870f99d91b78b1310fde329a59438d80
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-12-05 12:36:19 +0900

    [Bronze V] Title: Just Round Down, Time: 180 ms, Memory: 17892 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.java
@@ -0,0 +1,13 @@
+// boj 35428 Just Round Down
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double n = sc.nextDouble();
+		int nInt = (int)n;
+		System.out.println(nInt);
+	}
+}
