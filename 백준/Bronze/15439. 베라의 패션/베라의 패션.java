62c886ff7fc08512dfe7593c535f47a541e39268
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-12-08 12:25:22 +0900

    [Bronze IV] Title: 베라의 패션, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,12 @@
+// boj 15439 베라의 패션
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N * (N - 1));
+	}
+}
