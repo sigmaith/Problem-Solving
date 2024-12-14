d0a863347a69bd9b6e8a3d9458c562605fd622f9
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-12-14 09:07:58 +0900

    [Bronze IV] Title: 녹색거탑, Time: 176 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,11 @@
+// boj 24723 녹색거탑
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println((int)Math.pow(2, N));
+    }
+}
