66340b5f25b416d739c49ba9f4c5dd4e610d7bd2
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-12-18 22:40:29 +0900

    [Bronze IV] Title: Hello Judge, Time: 192 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+		    System.out.printf("Hello World, Judge %d!\n", i);
+		}
+	}
+}
