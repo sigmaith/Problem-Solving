ca3b70f7c93b7232ec6c297ba90c9e04c48fce8e
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2024-12-29 17:45:21 +0900

    [Bronze IV] Title: 줄번호, Time: 124 ms, Memory: 16192 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,18 @@
+// boj 2270 줄번호
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            bw.write(i + ". " + br.readLine() + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
